2461807822c55e0816c2675ce9e9314a60340962
Defri Indra Mahardika <defrindr@gmail.com>
2024-08-09 14:35:30 +0700

    add: admin template & dashboard

---
+++ resources/views/admin/layouts/_sidebar/author.blade.php
@@ -0,0 +1,16 @@
+<li class="nav-item">
+    <a href="{{ route('theme.index') }}" class="nav-link">
+        <i class="nav-icon bi bi-speedometer"></i>
+        <p>
+            Manajemen Judul
+        </p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('ebook.me') }}" class="nav-link">
+        <i class="nav-icon bi bi-speedometer"></i>
+        <p>
+            Karya Saya
+        </p>
+    </a>
+</li>
