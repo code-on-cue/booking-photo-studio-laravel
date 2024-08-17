d806fb4070b13d1ca8b012eba385c67fd058d26f
Defri Indra Mahardika <defrindr@gmail.com>
2024-08-17 23:50:57 +0700

    feat: specific role to admin & customer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,9 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Customer']);
         // User::factory(10)->create();
         $this->call(ConfigSeeder::class);
         User::factory()->create([
+            'role_id' => 1,
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
